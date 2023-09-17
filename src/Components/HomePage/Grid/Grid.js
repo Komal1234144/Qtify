a2a32a62a823e96eadaa5a939c5579cf84375c99
unknown <tolambia.komal5@gmail.com>
2023-09-17 21:40:33 +0530

    tabsFilter-qtify-takehome

--- src/Components/HomePage/Grid/Grid.js
+++ src/Components/HomePage/Grid/Grid.js
@@ -3,11 +3,50 @@ import './Grid.scss';
 import axios from 'axios';
 import { BASE_URL } from '../../../utils/url';
 import AlbumCard from '../AlbumCard/AlbumCard';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Pagination, Navigation } from 'swiper/modules';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 const Grid = ({ type, name }) => {
 
 
   const [songs, setSongs] = useState([]);
+  const [swiperRef, setSwiperRef] = useState(null);
+  const [value, setValue] = React.useState(0);
+  const [genres, setGenres] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/genres`,
+      {
+        Content: 'application/json',
+      })
+      .then((response) => {
+        console.log(response)
+        if(response.status === 200){
+          setGenres(response?.data?.data)
+        }
+
+      })
+  }, [])
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
 
   useEffect(() => {
 
@@ -17,7 +56,7 @@ const Grid = ({ type, name }) => {
       url = `${BASE_URL}/albums/top`
     } else if (type === 'new') {
       url = `${BASE_URL}/albums/new`
-    }else if(type === 'songs'){
+    } else if (type === 'songs') {
       url = `${BASE_URL}/songs`
     }
 
@@ -42,12 +81,67 @@ const Grid = ({ type, name }) => {
       <div className='grid'>
 
         {
+          type === 'songs'
+          &&
+          <Box sx={{ width: '100%' , marginBottom : '20px' }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+              <Tabs
+              textColor='white'
+              TabIndicatorProps={{style: {background:'#34c94b'}}}
+              value={value} onChange={handleChange} aria-label="basic tabs example">
+                <Tab label="All" 
+                 sx={{
+                  color : 'white',
+                  fontSize : '15px',
+                  textTransform : 'capitalize'
+                }}
+                {...a11yProps(0)} />
+                {
+                  genres?.map((genre, index) => {
+                    return <Tab label={genre.label}
+                    sx={{
+                      color : 'white',
+                      fontSize : '15px',
+                      textTransform : 'capitalize'
+                    }}
+                    {...a11yProps(index + 1)} />
+                  })
+                }
+              </Tabs>
+            </Box>
+          </Box>
+        }
+
+        <Swiper
+          onSwiper={setSwiperRef}
+          slidesPerView={7}
+          centeredSlides={false}
+          spaceBetween={2}
+          pagination={{
+            type: 'fraction',
+          }}
+          navigation={true}
+          modules={[Navigation]}
+          className="mySwiper"
+        >
+          {
+            songs?.map((song, index) => {
+              return <SwiperSlide>
+                <div className='grid__card'>
+                  <AlbumCard key={index} title={song?.title} image={song?.image} follows={song?.follows} likes={song?.likes} />
+                </div>
+              </SwiperSlide>
+            })
+          }
+        </Swiper>
+
+        {/* {
           songs?.map((song, index) => {
             return <div className='grid__card'>
-              <AlbumCard key={index} title={song?.title} image={song?.image} follows={song?.follows} likes={song?.likes}/>
+              <AlbumCard key={index} title={song?.title} image={song?.image} follows={song?.follows} likes={song?.likes} />
             </div>
           })
-        }
+        } */}
 
       </div>
     </>
