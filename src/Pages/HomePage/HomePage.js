7813612f7e66154f1509dbf8cbc791946d011389
unknown <tolambia.komal5@gmail.com>
2023-09-17 12:28:56 +0530

    grid-qtify-takehome

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './HomePage.scss';
 import Hero from '../../Components/HomePage/Hero/Hero';
-import AlbumCard from '../../Components/HomePage/AlbumCard/AlbumCard';
 import Grid from '../../Components/HomePage/Grid/Grid';
 
 
@@ -10,11 +9,11 @@ const HomePage = () => {
         <div className='home'>
             <Hero />
 
-            {/* <div className='p-5'>
-                <AlbumCard />
-            </div> */}
-
             <Grid type="top" name="Top Albums"/>
+
+            <hr/>
+
+            <Grid type="new" name="New Albums"/>
 
         </div>
     )
